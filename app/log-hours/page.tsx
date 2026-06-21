45f8b65bbdcc0d8a761d8f5b404f2a216b6b99fb
Noriel Villareal <norieldvillareal@gmail.com>
2026-06-22 01:07:19 +0800

    import working version

--- app/log-hours/page.tsx
+++ app/log-hours/page.tsx
@@ -129,7 +129,7 @@ if (loading) {
         <div className="bg-white p-6 rounded-xl shadow-xl border">
 
           <h1 className="text-2xl font-semibold mb-2">
-            File OT
+            File Overtime
           </h1>
 
           <p className="text-sm text-gray-700 mb-2">
